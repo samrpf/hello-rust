2923ff4a563890012cae440a8f77f5a4acd5bb05
samr <samar.thind@outlook.com>
2022-10-12 06:58:55 -0700

    Made file?

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+// my very good program
+// in Rust!
+// by samrcode on GitHub
+
+extern crate ferris_says;
+
+use ferris_says::say;
+use std::io::{ stdout, BufWriter };
+use std::time::Duration;
+use std::thread::sleep;
+
+// main function
+fn main() {
+    println!("Hello, world!");
+    println!("Welcome to the Employee Console!");
+    println!(); // print newline
+
+    // create employees
+    let employee1 = Employee {
+        id: 37281,
+        name: String::from("Bean")
+    };
+    let employee2 = Employee {
+        id: 37282,
+        name: String::from("Other Bean")
+    };
+    let employee3 = Employee {
+        id: 37283,
+        name: String::from("Another Bean")
+    };
+
+    // put employees in a bunch
+    let bunch_of_employees = [employee1, employee2, employee3];
+
+    // Make pause info stuffs
+    let duration_to_sleep = Duration::from_secs(3);
+
+    // Print employee information
+    for employee in bunch_of_employees {
+        println!("NEW EMPLOYEE!");
+        println!("ID: {}", employee.id);
+        println!("Name: {}", employee.name);
+        println!(); // print newline after
+        sleep(duration_to_sleep); // then sleep for a few seconds
+    }
+
+    // Done with employees
+    println!("No new employees are appearing.");
+    println!("You can now see your employees.");
+
+    // create message
+    let message =  b"Hello fellow bean friend";
+    let width = 24;
+
+    // create writer
+    let mut writer = BufWriter::new(stdout());
+
+    // start loop
+    for _n in 1..3 {
+        say(message, width, &mut writer).unwrap();
+    }
+}
+
+// create Employee struct
+struct Employee {
+    id: i32,
+    name: String
+}
